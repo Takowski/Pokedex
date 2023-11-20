aca0c96f87aab96c63292f9ad257866025434709
Mardixx <kimi.lefort.pro@gmail.com>
2023-11-20 14:43:11 +0100

    Design and condition

--- views/index.php
+++ views/index.php
@@ -10,6 +10,7 @@ require_once __DIR__.'/partials/header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pokédex - Homepage</title>
     <link rel="stylesheet" href="../sass/style.css">
+    <link rel="icon" type="image/x-icon" href="./pokeball.png">
 </head>
 <body>
 <main>
