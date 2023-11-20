43b67a99b9058a2997f2d08ad78b84b0c4683965
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-20 15:39:49 +0100

    display star if favorite in the view pokemon

--- views/show.php
+++ views/show.php
@@ -1,10 +1,15 @@
-<?php 
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
 $title = $_GET['name'];
 
-require_once __DIR__.'/partials/header.php';
+require_once __DIR__ . '/partials/header.php';
 require './data/db.php';
 
 ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,49 +18,74 @@ require './data/db.php';
 </head>
 
 <main>
-    
-    <?php 
-        $query = "SELECT * FROM Pokemon WHERE name = '$title'";
-        $fetch = $bdd->query($query)->fetchAll(PDO::FETCH_ASSOC);
-        foreach($fetch as $infos) {
-            $id = $infos["id"];
-            $name = str_replace(["'", ".", " "], "", strtolower($infos["name"]));
-            echo <<<EOD
+
+    <?php
+    $query = "SELECT * FROM Pokemon WHERE name = '$title'";
+    $fetch = $bdd->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($fetch as $infos) {
+        $id = $infos["id"];
+        $name = str_replace(["'", ".", " "], "", strtolower($infos["name"]));
+        echo <<<EOD
                 <img src="../public/img/pokemon/$name.png" alt="$name Img" width="500px"><br />
             EOD;
-        }
+    }
     ?>
+    <?php
+    // check if the pokemon is in the user's favorites, if so, display a yellow star, if not, display a blueish star
+    require_once './controllers/accessdb.php';
 
+    $userId = $_SESSION['user_id'];
+
+
+    $stmt = $pdo->prepare('SELECT Favorites FROM users WHERE id = ?');
+    $stmt->execute([$userId]);
+    $user = $stmt->fetch();
+
+
+    if ($user && !empty($user['Favorites'])) {
+
+        $favorites = json_decode($user['Favorites'], true);
+
+
+        $isFavorite = in_array(strtolower($title), array_map('strtolower', $favorites));
+    } else {
+
+        $isFavorite = false;
+    }
+    // end of if the pokemon is in the user's favorites, display a yellow star, if not, display a blueish star
+    
+    ?>
     <div class="pokemonStat">
         <div class="favourite">
-            
+            <!-- code to display yellow and blueish star function of isFavorite var -->
+            <img src="../public/img/FavoriteStar/<?php echo $isFavorite ? 'starYellow.svg' : 'starBlueish.svg'; ?>" alt="<?php echo $isFavorite ? 'favorite' : 'not favorite'; ?>" width="50px">
         </div>
         <?php
-            $query = "SELECT * FROM Pokemon WHERE name = '$title'";
-            $fetch = $bdd->query($query)->fetchAll(PDO::FETCH_ASSOC);
-            foreach($fetch as $infos) {
-                $type1 = $infos["type1"];
-                $type2 = $infos["type2"];
+        $query = "SELECT * FROM Pokemon WHERE name = '$title'";
+        $fetch = $bdd->query($query)->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($fetch as $infos) {
+            $type1 = $infos["type1"];
+            $type2 = $infos["type2"];
 
-                echo <<<EOD
+            echo <<<EOD
                 <h1>{$infos["name"]}</h1> 
                 EOD;
-                if($type2 != "NULL") {
-                    echo <<<EOD
+            if ($type2 != "NULL") {
+                echo <<<EOD
                     <div class="types">
                         <span class=$type1>$type1</span>
                         <span class=$type2>$type2</span>
                     </div>
                     EOD;
-                } else {
-                    echo <<<EOD
+            } else {
+                echo <<<EOD
                     <div class="types">
                         <span class=$type1>$type1</span>
                     </div>
                     
                     EOD;
-                }  
-                    echo <<<EOD
+            }
+            echo <<<EOD
                     <div class="pokeContent">
                         <ul class="pokeStat">
                             <li> HP: {$infos["hp"]}</li>
@@ -83,46 +113,40 @@ require './data/db.php';
                         </ul>
                     </div>
                 EOD;
-            }
+        }
         ?>
     </div>
     <h1>Evolution</h1>
     <div class="pokemonEvo">
         <?php
-            if ($infos['evolution'] == "2") 
-            {
-                $query = "SELECT * FROM Pokemon WHERE id >= '$id' ORDER BY id ASC LIMIT 3";
-                $fetch = $bdd->query($query)->fetchAll(PDO::FETCH_ASSOC);
-                foreach($fetch as $infos) 
-                {
-                    echo <<<EOD
-                        <img class="evoImg" src="../public/img/pokemon/{$infos["name"]}.png" alt="{$infos["name"]} Img" width="100px"><br />
-                        <p class="pokeName">{$infos["name"]}</p>
-                    EOD;
-                }            
-            } 
-            else if ($infos['evolution'] == "1")
-            {
-                $query = "SELECT * FROM Pokemon WHERE id >= '$id' ORDER BY id ASC LIMIT 2";
-                $fetch = $bdd->query($query)->fetchAll(PDO::FETCH_ASSOC);
-                foreach($fetch as $infos) {
-                    echo <<<EOD
-                        <img class="evoImg" src="../public/img/pokemon/{$infos["name"]}.png" alt="{$infos["name"]} Img" width="100px"><br />
-                        <p class="pokeName">{$infos["name"]}</p>
-                    EOD;
-                }
-            } 
-            else
-            {
+        if ($infos['evolution'] == "2") {
+            $query = "SELECT * FROM Pokemon WHERE id >= '$id' ORDER BY id ASC LIMIT 3";
+            $fetch = $bdd->query($query)->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($fetch as $infos) {
                 echo <<<EOD
+                        <img class="evoImg" src="../public/img/pokemon/{$infos["name"]}.png" alt="{$infos["name"]} Img" width="100px"><br />
+                        <p class="pokeName">{$infos["name"]}</p>
+                    EOD;
+            }
+        } else if ($infos['evolution'] == "1") {
+            $query = "SELECT * FROM Pokemon WHERE id >= '$id' ORDER BY id ASC LIMIT 2";
+            $fetch = $bdd->query($query)->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($fetch as $infos) {
+                echo <<<EOD
+                        <img class="evoImg" src="../public/img/pokemon/{$infos["name"]}.png" alt="{$infos["name"]} Img" width="100px"><br />
+                        <p class="pokeName">{$infos["name"]}</p>
+                    EOD;
+            }
+        } else {
+            echo <<<EOD
                     <p class="evoError"> This pokemon doesn't evolve</p>
                 EOD;
-            }
-           
+        }
+
         ?>
     </div>
 </main>
 
-<?php 
-require_once __DIR__.'/partials/footer.php';
+<?php
+require_once __DIR__ . '/partials/footer.php';
 ?>
